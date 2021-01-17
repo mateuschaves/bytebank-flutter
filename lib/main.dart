b57401631a27436ac63c85866802bd65172b0c10
Mateus Henrique Chaves Araujo Morais <mateushenriquechaves1@gmail.com>
2021-01-17 06:29:47 -0300

    Implementando o design do item de transferência

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,27 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Transferências'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-        ),
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Transferências'),
       ),
-    ));
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+      ),
+      body: Column(
+        children: <Widget>[
+          Card(
+            child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('R\$100.00'),
+                subtitle: Text('Mateus Henrique')),
+          ),
+          Card(
+            child: ListTile(
+                leading: Icon(Icons.monetization_on),
+                title: Text('R\$100.00'),
+                subtitle: Text('Mateus Henrique')),
+          ),
+        ],
+      ),
+    )));
