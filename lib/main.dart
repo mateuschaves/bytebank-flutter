31828c932796545a8eccb857c5c656bc14d09796
Mateus Henrique Chaves Araujo Morais <mateushenriquechaves1@gmail.com>
2021-01-17 07:07:26 -0300

    Habilitando o hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,51 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-        home: Scaffold(
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: FormularioTransferencia(),
+      ),
+    );
+  }
+}
+
+class FormularioTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Criando Transferência'),
+        ),
+        body: Text('Teste'));
+  }
+}
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text('Transferências'),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
       ),
-      body: ListaTransferencia(),
-    )));
-
-class ListaTransferencia extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        ItemTransferencia(
-          nome: 'Mateus Henrique',
-          valor: 'R\$ 100',
-        ),
-        ItemTransferencia(
-          nome: 'Isabela Simões',
-          valor: 'R\$ 20.000',
-        ),
-      ],
+      body: Column(
+        children: <Widget>[
+          ItemTransferencia(
+            nome: 'Mateus Henrique',
+            valor: 'R\$ 100',
+          ),
+          ItemTransferencia(
+            nome: 'Isabela Simões',
+            valor: 'R\$ 20.000',
+          ),
+        ],
+      ),
     );
   }
 }
