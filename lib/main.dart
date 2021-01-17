5a60b4f14d0459f291a1fe08da7f930c07805d95
Mateus Henrique Chaves Araujo Morais <mateushenriquechaves1@gmail.com>
2021-01-17 06:50:48 -0300

    Refatorando o código

--- lib/main.dart
+++ lib/main.dart
@@ -8,20 +8,44 @@ void main() => runApp(MaterialApp(
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
       ),
-      body: Column(
-        children: <Widget>[
-          Card(
-            child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('R\$100.00'),
-                subtitle: Text('Mateus Henrique')),
-          ),
-          Card(
-            child: ListTile(
-                leading: Icon(Icons.monetization_on),
-                title: Text('R\$100.00'),
-                subtitle: Text('Mateus Henrique')),
-          ),
-        ],
-      ),
+      body: ListaTransferencia(),
     )));
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        ItemTransferencia(
+          nome: 'Mateus Henrique',
+          valor: 'R\$ 100',
+        ),
+        ItemTransferencia(
+          nome: 'Isabela Simões',
+          valor: 'R\$ 20.000',
+        ),
+      ],
+    );
+  }
+}
+
+class ItemTransferencia extends StatelessWidget {
+  ItemTransferencia({
+    @required this.valor,
+    @required this.nome,
+  });
+
+  final String valor;
+  final String nome;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Card(
+      child: ListTile(
+          leading: Icon(Icons.monetization_on),
+          title: Text(valor),
+          subtitle: Text(nome)),
+    );
+  }
+}
